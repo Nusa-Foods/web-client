9d9cdc432810b02fe4273fccd3b05f5c55cbfd0b
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-05 04:54:41 +0700

    feat: update style on discover

--- src/app/(main)/discover/page.tsx
+++ src/app/(main)/discover/page.tsx
@@ -1,18 +1,25 @@
 import RecipeCard from "@/components/RecipeCard";
+import RecipeCardDetail from "@/components/RecipeCardDetail";
 
 export default function Discover() {
     return (
-        <main className="flex-grow p-6 bg-[#F9FAFB]">
-            <h1 className="text-3xl font-bold mb-6">
-                Discover Amazing Recipes
-            </h1>
-
-            <div className="grid grid-cols-1 gap-6 justify-items-center">
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
+        <div className="min-h-screen flex flex-col md:flex-row">
+            <div>
+                <main className="flex-grow p-6">
+                    <h1 className="text-3xl font-bold mb-6">
+                        Discover Amazing Recipes
+                    </h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 justify-items-center">
+                        <div className="grid grid-cols-1 gap-6 justify-items-center">
+                            <RecipeCard />
+                            <RecipeCard />
+                            <RecipeCard />
+                            <RecipeCard />
+                        </div>
+                        <RecipeCardDetail />
+                    </div>
+                </main>
             </div>
-        </main>
+        </div>
     );
 }
