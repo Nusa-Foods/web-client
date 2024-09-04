04f48d13f023af9c6bdc06b25e122d5ca114364e
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-05 04:35:17 +0700

    fix: styling on discover

--- src/app/(main)/discover/details/page.tsx
+++ src/app/(main)/discover/details/page.tsx
@@ -1,25 +1,22 @@
 import RecipeCardDetail from "@/components/RecipeCardDetail";
-import SideBar from "@/components/SideBar";
 
 export default function Discover() {
     return (
         <div className="min-h-screen flex flex-col md:flex-row">
-            {/* Sidebar */}
-            <SideBar />
-
             <div>
                 {/* Banner */}
-                <div className="relative h-25 sm:h-32 md:h-48 lg:h-64">
+                <div className="relative h-25 sm:h-32 md:h-48 lg:h-64 hidden md:block">
                     <img
                         src="https://storage.googleapis.com/chef-gpt.appspot.com/recipes%2FcVoeu0HZhjZhpPAZpyWy%2FcVoeu0HZhjZhpPAZpyWy.jpg"
                         alt="Recipe 1"
                         className="absolute inset-0 w-full h-full object-cover"
                     />
                     <div className="absolute inset-0 flex items-center justify-left p-4">
-                        <h2 className="text-8xl font-bold text-white">Banana Muffins</h2>
+                        <h2 className="text-8xl font-bold text-white">
+                            Banana Muffins
+                        </h2>
                     </div>
                 </div>
-
 
                 {/* Main content */}
                 <main className="flex-grow p-6">
--- src/app/(main)/discover/page.tsx
+++ src/app/(main)/discover/page.tsx
@@ -8,7 +8,7 @@ export default function Discover() {
                 Discover Amazing Recipes
             </h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 justify-items-center">
+            <div className="grid grid-cols-1 gap-6 justify-items-center">
                 <RecipeCard />
                 <RecipeCard />
                 <RecipeCard />
