5fe76d145fe9bc3ff27766b718deb23c485b7bed
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-10 10:25:40 +0700

    fix: recipe details layout

--- src/components/RecipeCardDetail.tsx
+++ src/components/RecipeCardDetail.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { RecipeType, UserType } from "@/type";
-import { MouseEvent, useEffect, useRef, useState } from "react";
+import { RecipeType } from "@/type";
+import { MouseEvent, useRef, useState } from "react";
 import revalidate from "@/actions";
 import showToast from "@/utils/toast";
 import CommentCard from "./CommentCard";
@@ -18,27 +18,7 @@ export default function RecipeCardDetail({
     recipeDetail: RecipeType;
     fetchRecipes?: () => void | Promise<void>;
 }) {
-    const [user, setUser] = useState<UserType>({});
-    const getUser = async (authorId: string) => {
-        const response = await fetch(
-            `${process.env.NEXT_PUBLIC_BASE_URL}/user/${authorId}`,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-
-        const data = await response.json();
-        setUser(data);
-    };
-
-    useEffect(() => {
-        getUser(String(recipeDetail.authorId));
-    }, []);
-
     const [text, setComment] = useState("");
-
     const inputRef = useRef<HTMLInputElement>(null);
 
     const handleComment = async (event: MouseEvent<SVGSVGElement>) => {
@@ -89,7 +69,7 @@ export default function RecipeCardDetail({
                 <img
                     src={recipeDetail.imgUrl}
                     alt={recipeDetail.title}
-                    className="rounded-lg object-cover w-full"
+                    className="rounded-lg object-cover w-full overflow-hidden"
                 />
             </div>
 
@@ -115,28 +95,27 @@ export default function RecipeCardDetail({
                                 : 0}
                         </p>
                     </div>
+                </div>
 
-                    <div className="flex justify-end gap-4">
-                        <div className="flex items-center space-x-2">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="#603F26"
-                                className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-                                />
-                            </svg>
-                        </div>
-
-                        <div className="flex items-center space-x-2">
-                            <ButtonAddBookmarks slug={recipeDetail.slug} />
-                        </div>
+                <div className="flex justify-center">
+                    <div className="flex items-center space-x-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="#603F26"
+                            className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                            />
+                        </svg>
+                        <p className="text-xs sm:text-sm md:text-md mr-2 sm:mr-4 md:mr-6">
+                            name ini belum aad
+                        </p>
                     </div>
                 </div>
 
@@ -150,63 +129,59 @@ export default function RecipeCardDetail({
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div>
-                    <div className="p-4">
-                        <h1 className="text-4xl font-bold capitalize">
-                            {recipeDetail.title}
-                        </h1>
-                        <p className="text-gray-600">
-                            {recipeDetail.description}
-                        </p>
-                        {recipeDetail.guide && (
-                            <Editor
-                                onChange={() => {}}
-                                initialContent={recipeDetail.guide}
-                                editable={false}
-                                // onGetContent={handleEditorChange} // Pass callback
-                            />
-                        )}
-                    </div>
-
-                    <div className="flex-col">
-                        {recipeDetail.comments &&
-                            recipeDetail.comments.map((el) => {
-                                return (
-                                    <CommentCard comment={el} key={el._id} />
-                                );
-                            })}
-                    </div>
-
-                    {/* //write comment */}
-                    <div className="flex gap-3 mt-5 justify-end">
-                        <input
-                            id="name"
-                            name="name"
-                            type="text"
-                            placeholder="add comment ..."
-                            required
-                            value={text}
-                            onChange={(e) => setComment(e.target.value)}
-                            ref={inputRef}
-                            className="block w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+            <div>
+                <div className="p-4">
+                    <h1 className="text-4xl font-bold capitalize">
+                        {recipeDetail.title}
+                    </h1>
+                    <p className="text-gray-600">{recipeDetail.description}</p>
+                    {recipeDetail.guide && (
+                        <Editor
+                            onChange={() => {}}
+                            initialContent={recipeDetail.guide}
+                            editable={false}
+                            // onGetContent={handleEditorChange} // Pass callback
                         />
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="#603F26"
-                            className="size-8"
-                            onClick={handleComment}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-                            />
-                        </svg>
-                    </div>
+                    )}
+                </div>
+
+                <div className="flex-col">
+                    {recipeDetail.comments &&
+                        recipeDetail.comments.map((el) => {
+                            return <CommentCard comment={el} key={el._id} />;
+                        })}
+                </div>
+
+                {/* //write comment */}
+                <div className="flex gap-3 mt-5 justify-end">
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="add comment ..."
+                        required
+                        value={text}
+                        onChange={(e) => setComment(e.target.value)}
+                        ref={inputRef}
+                        className="block w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                    />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="#603F26"
+                        className="size-8"
+                        onClick={handleComment}
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                        />
+                    </svg>
                 </div>
             </div>
         </div>
