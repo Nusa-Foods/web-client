eb4b4c0b9aee5df9108f32ed46bcbc49767ab31a
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-10 10:16:48 +0700

    fix: dropdown conditional rendering

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import mainLogo from "../../public/mainLogo.png";
+import ProfilDropdown from "./ProfilDropdown";
 
 export default function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -39,84 +40,74 @@ export default function NavBar() {
             <nav className="bg-white">
                 <div className="container mx-auto px-6 py-4 flex justify-between items-center">
                     {/* Logo */}
-                    <div className="flex items-center gap-10 ">
-                        <div className="flex flex-row justify-center items-center">
-                            <Image
-                                src={mainLogo}
-                                alt="Nusa Food Logo"
-                                className="w-14 h-14"
-                            />
-                            <span className="text-2xl font-semibold text-[#1F2937]">
-                                Nusa{" "}
-                                <span className="text-[#3A2D18]">Food</span>
-                            </span>
-                        </div>
+                    <div className="flex items-center gap-10 w-full justify-between">
+                        <div className="flex gap-10">
+                            <div className="flex flex-row justify-center items-center">
+                                <Image
+                                    src={mainLogo}
+                                    alt="Nusa Food Logo"
+                                    className="w-14 h-14"
+                                />
+                                <span className="text-2xl font-semibold text-[#1F2937] flex">
+                                    Nusa{" "}
+                                    <span className="text-[#3A2D18]">Food</span>
+                                </span>
+                            </div>
 
-                        {/* Navigation Links */}
-                        <div className="hidden lg:flex space-x-6 text-gray-500 justify-between items-center">
-                            <Link href="/nusa-recipes" className="nav-link">
-                                Nusa Recipes
-                            </Link>
-                            <Link href="/discover" className="nav-link">
-                                Discover
-                            </Link>
-                            <Link href="/your-chef" className="nav-link">
-                                Your Personal Chef
-                            </Link>
-                        </div>
-
-                        {/* Navigation Links */}
-                        <div className="space-x-6 hidden md:flex text-gray-500">
-                            <Link href="/nusa-recipes" className="nav-link">
-                                Nusa Recipes
-                            </Link>
-                            <Link href="/discover" className="nav-link">
-                                Discover
-                            </Link>
-                            <Link href="/your-chef" className="nav-link">
-                                Your Personal Chef
-                            </Link>
-                        </div>
-
-                        {!auth && (
-                            <div className="hidden lg:flex space-x-6 text-gray-500 justify-between items-center">
-                                <Link
-                                    href={"/login"}
-                                    className="w-20 bg-white font-medium py-2 rounded-md"
-                                >
-                                    Sign In
+                            {/* Navigation Links */}
+                            <div className="hidden lg:flex space-x-6 text-gray-500 justify-between items-center w-full">
+                                <Link href="/nusa-recipes" className="nav-link">
+                                    Nusa Recipes
                                 </Link>
-                                <Link
-                                    href={"/register"}
-                                    className="w-20 bg-custom-brown-1 text-center text-white font-medium py-2 rounded-md hover:bg-custom-brown-2 focus:outline-none focus:ring-2 focus:ring-custom-brown-4"
-                                >
-                                    Sign Up
+                                <Link href="/discover" className="nav-link">
+                                    Discover
+                                </Link>
+                                <Link href="/your-chef" className="nav-link">
+                                    Your Personal Chef
                                 </Link>
                             </div>
-                        )}
-                        {/* Mobile Menu Button */}
-                        <div className="lg:hidden flex items-center gap-3">
-                            <button
-                                onClick={toggleMenu}
-                                className="text-gray-500 focus:outline-none"
-                                aria-label="Toggle menu"
-                            >
-                                <svg
-                                    className="w-6 h-6"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16m-7 6h7"
-                                    ></path>
-                                </svg>
-                            </button>
                         </div>
+                        <ProfilDropdown />
+                    </div>
+
+                    {!auth && (
+                        <div className="hidden lg:flex space-x-6 text-gray-500 justify-between items-center">
+                            <Link
+                                href={"/login"}
+                                className="w-20 bg-white font-medium py-2 rounded-md"
+                            >
+                                Sign In
+                            </Link>
+                            <Link
+                                href={"/register"}
+                                className="w-20 bg-custom-brown-1 text-center text-white font-medium py-2 rounded-md hover:bg-custom-brown-2 focus:outline-none focus:ring-2 focus:ring-custom-brown-4"
+                            >
+                                Sign Up
+                            </Link>
+                        </div>
+                    )}
+                    {/* Mobile Menu Button */}
+                    <div className="lg:hidden flex items-center gap-3">
+                        <button
+                            onClick={toggleMenu}
+                            className="text-gray-500 focus:outline-none"
+                            aria-label="Toggle menu"
+                        >
+                            <svg
+                                className="w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16m-7 6h7"
+                                ></path>
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </nav>
