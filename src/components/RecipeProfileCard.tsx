f133adaeca473c797f3d6a82f222c9c7cec001d9
liliane673 <lilianeernestin673@gmail.com>
2024-09-11 09:47:38 +0700

    feat: wiring recipes by user id and show in the profile page

--- src/components/RecipeProfileCard.tsx
+++ src/components/RecipeProfileCard.tsx
@@ -2,11 +2,11 @@ import { RecipeType } from "@/type";
 import Link from "next/link";
 
 export default function RecipeProfileCard({ recipe }: { recipe: RecipeType }) {
-    console.log("ini recipe", recipe);
+    // console.log("ini recipe", recipe);
     return (
         <Link
             href={`/discover/${recipe.slug}`}
-            className="flex flex-col bg-white border rounded-lg shadow-md overflow-hidden"
+            className="flex flex-col bg-white border rounded-lg shadow-md overflow-hidden h-full"
         >
             <div className="w-full h-48 bg-gray-300 overflow-hidden">
                 <img
@@ -15,11 +15,27 @@ export default function RecipeProfileCard({ recipe }: { recipe: RecipeType }) {
                     className="object-cover w-full h-full transition-transform duration-300 ease-in-out transform hover:scale-110"
                 />
             </div>
-            <div className="p-4">
-                <h1 className="text-lg font-bold">{recipe.title}</h1>
-                <p className="text-md text-gray-600">{recipe.description}</p>
-                <p className="text-md text-gray-600">{recipe.likes?.length} likes</p>
+            <div className="flex flex-col justify-between flex-grow">
+                <div className="px-4 mt-2">
+                    <h1 className="text-lg font-bold">{recipe.title}</h1>
+                    <p className="text-md text-gray-600">{recipe.description}</p>
+                </div>
+                <div className="p-4 mt-auto flex items-center">
+                    <p className="text-md text-gray-600 mr-2">
+                        {recipe.likes?.length} {recipe.likes?.length > 1 ? "likes | " : "like | "}
+                    </p>
+                    <p className="text-md text-gray-600 mr-2">
+                        {recipe.comments?.length} {recipe.comments?.length > 1 ? "comments" : "comment"}
+                    </p>
+                    <p className="text-md text-gray-600 italic ml-auto">
+                        posted on {recipe.createdAt.slice(0, 10)}
+                    </p>
+                </div>
+
+
+
             </div>
         </Link>
     );
+
 }
